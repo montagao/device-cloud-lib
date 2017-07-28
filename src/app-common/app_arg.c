824fd75e4c0fc2d98d0efce0e1d3109c24193fa1
Paul Barrette <paul.barrette@windriver.com>
2017-07-28 11:02:40 -0400

    app_common: add arg-parse to support helper apps
    Add argument parsing routines as a library for use with helper apps
    like iot-relay, iot-update and iot-control.  app_common was part of
    HDC2, but only the arg-parse routines were ported.  More will be added
    as it is required.
    
    Signed-off-by: Paul Barrette <paul.barrette@windriver.com>

---
+++ src/app-common/app_arg.c
@@ -0,0 +1,351 @@
+/**
+ * @file
+ * @brief source file for argument parsing functionality for an application
+ *
+ * @copyright Copyright (C) 2016-2017 Wind River Systems, Inc. All Rights Reserved.
+ *
+ * @license The right to copy, distribute or otherwise make use of this software
+ * may be licensed only pursuant to the terms of an applicable Wind River
+ * license agreement.  No license to Wind River intellectual property rights is
+ * granted herein.  All rights not licensed by Wind River are reserved by Wind
+ * River.
+ */
+
+#include "app_arg.h"
+
+#include <os.h>
+
+unsigned int app_arg_count( const struct app_arg *args, char ch,
+	const char *name )
+{
+	unsigned int result = 0u;
+	int found = 0;
+	while ( !found && args && ( args->ch || args->name ) )
+	{
+		if ( ( ch != 0 && ch == args->ch ) ||
+			( name && args->name &&
+		 os_strcmp( name, args->name ) == 0 ) )
+		{
+			result = args->hit;
+			found = 1;
+		}
+		++args;
+	}
+	return result;
+}
+
+int app_arg_parse( struct app_arg *args, int argc, char **argv,
+	int *pos )
+{
+	int result = EXIT_SUCCESS;
+	int i;
+	char ch = '\0';
+	int pos_arg = 0;
+	const char *name = NULL;
+	const char *arg_id = NULL;
+	const char **next_arg = NULL;
+	struct app_arg *arg = args;
+	while ( arg && ( arg->ch || arg->name ) )
+	{
+		arg->hit = 0u;
+		++arg;
+	}
+	for ( i = 1u; i < argc && pos_arg == 0 && result == EXIT_SUCCESS; ++i )
+	{
+		const char *a = argv[i];
+		arg = args;
+		if ( next_arg )
+		{
+			if ( *a == '-' || *a == '\0' )
+			{
+				if ( arg_id && *arg_id == '[' )
+					next_arg = NULL;
+				else
+					result = EXIT_FAILURE;
+			}
+			else
+			{
+				*next_arg = a;
+				next_arg = NULL;
+			}
+		}
+		else if ( !next_arg && *a != '-' )
+			pos_arg = i;
+		if ( !next_arg && *a == '-' )
+		{
+			int handled = 0;
+			++a;
+			while ( arg && ( arg->ch || arg->name ) && !handled )
+			{
+				if ( *a == '-' )
+				{
+					++a;
+					if ( arg->name && os_strncmp( a, arg->name,
+					     os_strlen( arg->name ) ) == 0 )
+					{
+						a += os_strlen( arg->name );
+						name = arg->name;
+						handled = 1;
+					}
+				}
+				else if ( *a == arg->ch )
+				{
+					++a;
+					ch = arg->ch;
+					handled = 1;
+				}
+				if ( handled )
+				{
+					++arg->hit;
+					if ( arg->param_result )
+					{
+						if ( *a && *a == '=' )
+							++a;
+						if ( *a )
+							*arg->param_result = a;
+						else
+						{
+							next_arg =
+							    arg->param_result;
+							arg_id = arg->param;
+						}
+					}
+					else if ( *a )
+					{
+						os_fprintf( OS_STDERR,
+							"Unexpected value \"%s\" "
+							"for argument argument: ", a );
+						result = EXIT_FAILURE;
+					}
+				}
+				a = argv[i] + 1u;
+				++arg;
+			}
+			if ( !handled )
+			{
+				os_fprintf( OS_STDERR,
+					"unknown argument: %s\n", argv[i] );
+				ch = 0;
+				name = NULL;
+				result = EXIT_FAILURE;
+			}
+		}
+	}
+	if ( next_arg && ( !arg_id || *arg_id != '[' ) )
+	{
+		os_fprintf( OS_STDERR, "%s", "expected " );
+		if ( arg_id )
+			os_fprintf( OS_STDERR, "\"%s\" ", arg_id );
+		os_fprintf( OS_STDERR, "%s", "value for argument: " );
+		result = EXIT_FAILURE;
+	}
+
+	/* check for required arguments */
+	arg = args;
+	while ( result == EXIT_SUCCESS && arg && ( arg->ch || arg->name ) )
+	{
+		if ( !arg->hit && arg->req )
+		{
+			os_fprintf( OS_STDERR,"%s", 
+				"required argument not specified: " );
+			name = arg->name;
+			ch = arg->ch;
+			result = EXIT_FAILURE;
+		}
+		++arg;
+	}
+	if ( result == EXIT_FAILURE )
+	{
+		if ( ch )
+			os_fprintf( OS_STDERR, "-%c\n", ch );
+		else if ( name )
+			os_fprintf( OS_STDERR, "--%s\n", name );
+	}
+	else if ( !pos && pos_arg > 0 )
+	{
+		os_fprintf( OS_STDERR, "unknown argument: %s\n",
+			argv[pos_arg] );
+		result = EXIT_FAILURE;
+	}
+	else if ( pos )
+		*pos = pos_arg;
+	return result;
+}
+
+void app_arg_usage( const struct app_arg *args, size_t col,
+	const char *app, const char *desc, const char *pos,
+	const char *pos_desc )
+{
+	unsigned int i;
+	int has_type[] = { 0, 0 };
+	const struct app_arg *arg;
+	const char *app_name = "exec";
+	size_t pos_len = 0u;
+	if ( app )
+	{
+		app_name = os_strrchr( app, '/' );
+		if ( app_name )
+			++app_name;
+		else
+			app_name = app;
+	}
+	os_printf( "usage: %s", app_name );
+
+	arg = args;
+	while( arg && ( arg->ch || arg->name ) )
+	{
+		os_printf( "%s", " " );
+		if ( !arg->req )
+		{
+			os_printf( "%s", "[" );
+			++has_type[1];
+		}
+		else
+			++has_type[0];
+		if ( arg->ch )
+			os_printf( "-%c", arg->ch );
+		else
+			os_printf( "--%s", arg->name );
+		if ( arg->param )
+			os_printf( " %s", arg->param );
+		if ( !arg->req )
+			os_printf( "%s", "]" );
+		++arg;
+	}
+
+	/* handle positional argument display */
+	if ( pos )
+	{
+		int pos_opt = 0;
+		int pos_multi = 0;
+		pos_len = os_strlen( pos );
+		if ( pos[pos_len - 1u] == '+' )
+		{
+			--pos_len;
+			pos_multi = 1;
+		}
+		if ( *pos == '[' )
+		{
+			++pos;
+			pos_opt = 1;
+			pos_len -= 2u;
+		}
+		os_printf( "%s", " " );
+		if ( !pos_opt )
+		{
+			os_printf( "%.*s", (int)pos_len, pos );
+			if ( pos_multi )
+				os_printf( "%s", " " );
+		}
+		if ( pos_multi || pos_opt )
+			os_printf( "[%.*s", (int)pos_len, pos );
+		if ( pos_multi )
+			os_printf( "%s", " ..." );
+		if ( pos_multi || pos_opt )
+			os_printf( "%s", "]" );
+	}
+	os_printf( "%s", "\n" );
+	if ( desc )
+		os_printf( "\n%s\n", desc );
+	if ( pos )
+	{
+		os_printf( "\npositional arguments:\n%-.*s%*s",
+			(int)pos_len, pos, (int)(col - pos_len), "" );
+		if ( pos_desc )
+			os_printf( "%s", pos_desc );
+		os_printf( "%s", "\n" );
+	}
+
+	if ( col ) --col;
+	for ( i = 0u; i < 2u; ++i )
+	{
+		if ( has_type[i] )
+		{
+			os_printf( "%s", "\n" );
+			if ( i )
+				os_printf( "%s", "optional arguments:\n" );
+			else
+				os_printf( "%s", "required arguments:\n" );
+			arg = args;
+			while ( arg && ( arg->ch || arg->name ) )
+			{
+				if ( ( i && !arg->req ) || ( !i && arg->req ) )
+				{
+					size_t id_len = 0u;
+					size_t line_len = 0u;
+					/* calculate the size of the parameter id tag */
+					if ( arg->param )
+					{
+						id_len = col;
+						if ( arg->ch )
+							id_len -= 3u; /* "-c " */
+						if ( arg->name )
+							id_len -=
+							  ( os_strlen( arg->name ) + 3u ); /* "--name " */
+						if ( arg->ch && arg->name )
+						{
+							id_len -= 2u; /* ", " */
+							id_len /= 2u;
+						}
+						if ( id_len > os_strlen( arg->param ) ) /* arg*/
+							id_len = os_strlen( arg->param );
+					}
+					if ( arg->ch )
+					{
+						os_printf( "-%c", arg->ch );
+						line_len = 2u;
+						if ( arg->param )
+						{
+							os_printf( " %*.*s",
+								(int)id_len,
+								(int)id_len,
+								arg->param );
+							line_len += id_len + 1u;
+						}
+						if ( arg->name )
+						{
+							os_printf( "%s", ", " );
+							line_len += 2u;
+						}
+					}
+					if ( arg->name )
+					{
+						size_t max_name_len = col - line_len - 2u;
+						if ( arg->param )
+							max_name_len -= id_len - 1u; /* " " */
+						os_printf( "--%.*s",
+							(int)max_name_len,
+							arg->name );
+						if ( os_strlen( arg->name )
+							< max_name_len )
+							line_len +=
+								os_strlen( arg->name ) + 2u;
+						else
+							line_len += max_name_len + 2u;
+						if ( arg->param )
+						{
+							os_printf( " %*.*s",
+								(int)id_len,
+								(int)id_len,
+								arg->param );
+							line_len += id_len + 1u;
+						}
+					}
+					if ( line_len < col )
+					{
+						line_len = col - line_len;
+						os_printf( "%*.*s",
+							(int)line_len,
+							(int)line_len, " " );
+					}
+					if ( arg->desc )
+						os_printf( " %s",
+							arg->desc );
+					os_printf( "%s", "\n" );
+				}
+				++arg;
+			}
+		}
+	}
+}
+
---
+++ src/app-common/app_arg.h
@@ -0,0 +1,108 @@
+/**
+ * @file
+ * @brief header file for argument parsing functionality for an application
+ *
+ * @copyright Copyright (C) 2016-2017 Wind River Systems, Inc. All Rights Reserved.
+ *
+ * @license The right to copy, distribute or otherwise make use of this software
+ * may be licensed only pursuant to the terms of an applicable Wind River
+ * license agreement.  No license to Wind River intellectual property rights is
+ * granted herein.  All rights not licensed by Wind River are reserved by Wind
+ * River.
+ */
+
+#ifndef APP_ARG_H
+#define APP_ARG_H
+
+#include <stdlib.h> /* for size_t */
+
+/** @brief Structure defining an arugment for an application */
+struct app_arg
+{
+	/** @brief Argument single character token (i.e. specified with "-") */
+	char ch;
+	/** @brief Argument multi character token (i.e. specified with "--") */
+	const char *name;
+	/** @brief Whether the argument is required */
+	int req;
+	/** @brief Parameter id used in display */
+	const char *param;
+	/** @brief Location set pointer if found */
+	const char **param_result;
+	/** @brief Argument description */
+	const char *desc;
+	/** @brief Number of times argument was found */
+	unsigned int hit;
+};
+
+/**
+ * @brief Returns the number of times an argument was specified
+ *
+ * @param[in]      args                          pointer to the application
+ *                                               arguments array
+ * @param[in]      ch                            single character token
+ *                                               (optional)
+ * @param[in]      name                          multiple character token
+ *                                               (optional)
+ *
+ * @return the number of times that the argument was specified
+ *
+ * @see app_arg_parse
+ */
+unsigned int app_arg_count( const struct app_arg *args, char ch,
+	const char *name );
+/**
+ * @brief Parses arguments passed to the application
+ *
+ * @param[in,out]  args                          array specifying arguments
+ *                                               expected by the application
+ * @param[in]      argc                          number of arguments passed to
+ *                                               the application
+ * @param[in]      argv                          array of arguments passed to
+ *                                               the application
+ * @param[in,out]  pos                           index of first positional
+ *                                               argument (optional)
+ *
+ * @note This fucnction doesn't check for optional or required positional
+ * arguments
+ *
+ * @note If no positional arguments are found then @p pos will return a value
+ * of 0
+ *
+ * @retval EXIT_FAILURE                          application encountered an
+ *                                               error
+ * @retval EXIT_SUCCESS                          application completed
+ *                                               successfully
+ */
+int app_arg_parse( struct app_arg *args, int argc, char **argv,
+	int *pos );
+/**
+ * @brief Prints to stdout which arguments will be handled by the application
+ *
+ * @param[in]      args                          array specifying arguments
+ *                                               expected by the application
+ * @param[in]      col                           column index at which to align
+ *                                               argument descriptions
+ * @param[in]      app                           name of the application
+ *                                               (optional)
+ * @param[in]      desc                          description of the application
+ *                                               (optional)
+ * @param[in]      pos                           id name to use for positional
+ *                                               arguments (optional)
+ * @param[in]      pos_desc                      description for positional
+ *                                               arguments (optional)
+ *
+ * @note @b pos can be of the format "id", "[id]", "id+" or "[id]+", where 'id'
+ * is the name to use for the argument. Enclosing the id in brackets '[]'
+ * indicates that positional argument(s) are optional.  A plus '+' at the end
+ * indicates that multiple positional arguments are able to be handled.
+ *
+ * @retval 0                                    no errors encountered
+ * @retval 1                                    an error was encountered
+ */
+void app_arg_usage( const struct app_arg *args, size_t col,
+	const char *app, const char *desc, const char *pos,
+	const char *pos_desc );
+
+#endif /* ifndef APP_ARG_H */
+
