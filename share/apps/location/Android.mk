1cd4dcba4a86be24dce770f53ef6af3b04557998
Paul Barrette <paulbarrette@gmail.com>
2017-11-30 17:07:09 -0500

    android: harmonize license text
    Make sure the licensing text is the same as other files so that it can
    be maintained.
    
    Signed-off-by: Paul Barrette <paulbarrette@gmail.com>

--- share/apps/location/Android.mk
+++ share/apps/location/Android.mk
@@ -1,11 +1,10 @@
 #
 # Copyright (C) 2016 Wind River Systems, Inc. All Rights Reserved.
 #
-# The right to copy, distribute or otherwise make use of this software
-# may be licensed only pursuant to the terms of an applicable Wind River
-# license agreement.  No license to Wind River intellectual property rights is
-# granted herein.  All rights not licensed by Wind River are reserved by Wind
-# River.
+# The right to copy, distribute or otherwise make use of this software may
+# be licensed only pursuant to the terms of an applicable Wind River license
+# agreement.  No license to Wind River intellectual property rights is granted
+# herein.  All rights not licensed by Wind River are reserved by Wind River.
 #
 
 LOCAL_PATH:= $(call my-dir)
