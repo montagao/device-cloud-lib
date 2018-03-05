62227cdb38a46e15c6d572ab6a69269743774231
Keith Holman <keith.holman@windriver.com>
2018-03-05 13:33:38 -0500

    test: port argument parsing unit tests
    This patch is a port of the argument parsing unit tests from the old
    repository.  This helps ensure that the utility for argument parsing
    continues to work as expected.
    
    Signed-off-by: Keith Holman <keith.holman@windriver.com>

---
+++ test/unit/utilities/app_arg_test.c
@@ -0,0 +1,577 @@
+/**
+ * @file
+ * @brief unit testing for common functions (argument parsing file)
+ *
+ * @copyright Copyright (C) 2017-2018 Wind River Systems, Inc. All Rights Reserved.
+ *
+ * @license Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied."
+ */
+
+#include "test_support.h"
+
+#include "iot_build.h"
+#include "utilities/app_arg.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+static const char *ARG_VALUE[] = { NULL, NULL, NULL, NULL, NULL, NULL };
+static const struct app_arg TEST_ARGS[] = {
+	{ '1', "one",           0u,    NULL,    NULL,          "one description",   1u },
+	{ '2', "two",           1u,    NULL,    NULL,          "two required",      2u },
+	{ '3', "three",         0u,    "arg",   &ARG_VALUE[0], "three description", 3u },
+	{ '4', "four",          0u,    NULL,    &ARG_VALUE[1], "four description",  4u },
+	{ '5', "five",          0u,    "[arg]", &ARG_VALUE[2], "five description",  5u },
+	{ '6', "six",           0u,    "[arg]", &ARG_VALUE[3], "six description",   6u },
+	{ '7', "seven",         0u,    "arg",   &ARG_VALUE[4], "seven description", 7u },
+	{ '8', "eight",         0u,    NULL,    NULL,          "eight description", 8u },
+	{ '9', "nine",          0u,    NULL,    NULL,          "nine description",  9u },
+	{ 0u,  "name-only",     0u,    NULL,    NULL,          "name-only #1",      10u },
+	{ 0u,  "name-required", 1u,    NULL,    NULL,          "name-only #2",      11u },
+	{ 0u,  "name-with-arg", 0u,    "arg",   &ARG_VALUE[5], "name-only #3",      12u },
+	{ 0u, NULL, 0, NULL, NULL, NULL, 0u }
+};
+
+
+/* static helper functions */
+static void* internal_struct_copy( const void* struct_ptr, size_t item_size,
+	size_t item_count )
+{
+	void *result = NULL;
+	void *out_ptr = NULL;
+	assert_non_null( struct_ptr );
+	if ( item_count > 0u )
+	{
+		result = test_calloc( item_count, item_size );
+		assert_non_null( result );
+		out_ptr = result;
+		while ( item_count > 0u )
+		{
+			memcpy( out_ptr, struct_ptr, item_size );
+			struct_ptr = (const char *)struct_ptr + item_size;
+			out_ptr = (char*)out_ptr + item_size;
+
+			--item_count;
+		}
+	}
+	return result;
+}
+
+static void internal_struct_free( void* struct_ptr )
+{
+	if ( struct_ptr )
+		test_free( struct_ptr );
+}
+
+static char** internal_args_allocate( const char **args, int argc )
+{
+	char **result = NULL;
+	if ( argc > 0 )
+	{
+		result = test_malloc( sizeof( char * ) * argc );
+		assert_non_null( result );
+	}
+	while ( argc > 0 )
+	{
+		size_t str_len;
+		--argc;
+		str_len = strlen( args[argc] ) + 1u;
+		result[argc] = test_malloc( str_len );
+		assert_non_null( result[argc] );
+		strncpy( result[argc], args[argc], str_len );
+	}
+	return result;
+}
+
+static void internal_args_free( char **args, int argc )
+{
+	if ( args )
+	{
+		while( argc > 0 )
+		{
+			--argc;
+			test_free( args[ argc ] );
+		}
+		test_free( args );
+	}
+}
+
+
+/* app_arg_count */
+static void test_app_arg_count_found_by_id( void **state )
+{
+	unsigned int result;
+
+	result = app_arg_count( TEST_ARGS, '3', NULL );
+	assert_int_equal( result, 3u );
+}
+
+static void test_app_arg_count_found_by_name( void **state )
+{
+	unsigned int result;
+
+	result = app_arg_count( TEST_ARGS, 0, "three" );
+	assert_int_equal( result, 3u );
+}
+
+static void test_app_arg_count_no_id_or_name( void **state )
+{
+	unsigned int result;
+
+	result = app_arg_count( TEST_ARGS, 0, NULL );
+	assert_int_equal( result, 0 );
+}
+
+static void test_app_arg_count_null_obj( void **state )
+{
+	unsigned int result;
+
+	result = app_arg_count( NULL, 'c', "count" );
+	assert_int_equal( result, 0 );
+}
+
+
+/* app_arg_parse */
+static void test_app_arg_parse_argument_expected_value( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2", "--name-required",
+		"-3=three_value" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, NULL );
+	assert_int_equal( result, EXIT_SUCCESS );
+	assert_int_equal( opts[0].hit, 0u ); /* -1, --one */
+	assert_int_equal( opts[1].hit, 1u ); /* -2, --two */
+	assert_int_equal( opts[2].hit, 1u ); /* -3, --three */
+	assert_string_equal( ARG_VALUE[0], "three_value" );
+	assert_int_equal( opts[3].hit, 0u ); /* -4, --four */
+	assert_int_equal( opts[4].hit, 0u ); /* -5, --five */
+	assert_int_equal( opts[5].hit, 0u ); /* -6, --six */
+	assert_int_equal( opts[6].hit, 0u ); /* -7, --seven */
+	assert_int_equal( opts[7].hit, 0u ); /* -8, --eight */
+	assert_int_equal( opts[8].hit, 0u ); /* -9, --nine */
+	assert_int_equal( opts[9].hit, 0u ); /* --name-only */
+	assert_int_equal( opts[10].hit, 1u ); /* --name-required */
+	assert_int_equal( opts[11].hit, 0u ); /* --name-with-arg */
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_argument_no_value( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2", "--name-required",
+		"--three" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, NULL );
+	assert_int_equal( result, EXIT_FAILURE );
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_argument_optional_not_specified( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2", "--name-required",
+		"--five", "--six" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, NULL );
+	assert_int_equal( result, EXIT_SUCCESS );
+	assert_int_equal( opts[0].hit, 0u ); /* -1, --one */
+	assert_int_equal( opts[1].hit, 1u ); /* -2, --two */
+	assert_int_equal( opts[2].hit, 0u ); /* -3, --three */
+	assert_int_equal( opts[3].hit, 0u ); /* -4, --four */
+	assert_int_equal( opts[4].hit, 1u ); /* -5, --five */
+	assert_null( ARG_VALUE[2] );
+	assert_int_equal( opts[5].hit, 1u ); /* -6, --six */
+	assert_int_equal( opts[6].hit, 0u ); /* -7, --seven */
+	assert_int_equal( opts[7].hit, 0u ); /* -8, --eight */
+	assert_int_equal( opts[8].hit, 0u ); /* -9, --nine */
+	assert_int_equal( opts[9].hit, 0u ); /* --name-only */
+	assert_int_equal( opts[10].hit, 1u ); /* --name-required */
+	assert_int_equal( opts[11].hit, 0u ); /* --name-with-arg */
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_argument_optional_specified( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2", "--name-required",
+		"--five", "five_value" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, NULL );
+	assert_int_equal( result, EXIT_SUCCESS );
+	assert_int_equal( opts[0].hit, 0u ); /* -1, --one */
+	assert_int_equal( opts[1].hit, 1u ); /* -2, --two */
+	assert_int_equal( opts[2].hit, 0u ); /* -3, --three */
+	assert_int_equal( opts[3].hit, 0u ); /* -4, --four */
+	assert_int_equal( opts[4].hit, 1u ); /* -5, --five */
+	assert_string_equal( ARG_VALUE[2], "five_value" );
+	assert_int_equal( opts[5].hit, 0u ); /* -6, --six */
+	assert_int_equal( opts[6].hit, 0u ); /* -7, --seven */
+	assert_int_equal( opts[7].hit, 0u ); /* -8, --eight */
+	assert_int_equal( opts[8].hit, 0u ); /* -9, --nine */
+	assert_int_equal( opts[9].hit, 0u ); /* --name-only */
+	assert_int_equal( opts[10].hit, 1u ); /* --name-required */
+	assert_int_equal( opts[11].hit, 0u ); /* --name-with-arg */
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_argument_optional_specified_twice( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2", "--name-required",
+		"--five", "five_value", "five_value2" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, NULL );
+	assert_int_equal( result, EXIT_FAILURE );
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_argument_required( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2", "--name-required",
+		"--seven", "--eight" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, NULL );
+	assert_int_equal( result, EXIT_FAILURE );
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_argument_unexpected_value( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2", "--name-required",
+		"-8=eight_value" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, NULL );
+	assert_int_equal( result, EXIT_FAILURE );
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_argument_unknown_dash( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2", "--name-required",
+		"--unknown-arg" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, NULL );
+	assert_int_equal( result, EXIT_FAILURE );
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_argument_unknown_no_dash( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2", "--name-required",
+		"unknown-arg" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, NULL );
+	assert_int_equal( result, EXIT_FAILURE );
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_missing_required( void **state )
+{
+	const char *args[] = { "/path/to/app", "-2" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int pos = 0u;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, &pos );
+	assert_int_equal( result, EXIT_FAILURE );
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_mulitple( void **state )
+{
+	const char *args[] = { "/path/to/app", "--name-required",
+		"-2", "-2", "-2", "-2", "-2", "-2", "-2" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int pos = 0u;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, &pos );
+	assert_int_equal( result, EXIT_SUCCESS );
+	assert_int_equal( opts[0].hit, 0u ); /* -1, --one */
+	assert_int_equal( opts[1].hit, 7u ); /* -2, --two */
+	assert_int_equal( opts[2].hit, 0u ); /* -3, --three */
+	assert_int_equal( opts[3].hit, 0u ); /* -4, --four */
+	assert_int_equal( opts[4].hit, 0u ); /* -5, --five */
+	assert_int_equal( opts[5].hit, 0u ); /* -6, --six */
+	assert_int_equal( opts[6].hit, 0u ); /* -7, --seven */
+	assert_int_equal( opts[7].hit, 0u ); /* -8, --eight */
+	assert_int_equal( opts[8].hit, 0u ); /* -9, --nine */
+	assert_int_equal( opts[9].hit, 0u ); /* --name-only */
+	assert_int_equal( opts[10].hit, 1u ); /* --name-required */
+	assert_int_equal( opts[11].hit, 0u ); /* --name-with-arg */
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_null_obj( void **state )
+{
+	const char *args[] = { "/path/to/app" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int pos = 0u;
+	int result;
+
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( NULL, argc, argv, &pos );
+	assert_int_equal( result, EXIT_SUCCESS );
+
+	internal_args_free( argv, argc );
+}
+
+static void test_app_arg_parse_no_args( void **state )
+{
+	const char *args[] = { "/path/to/app" };
+	int argc = sizeof( args ) / sizeof( const char * );
+	char **argv;
+	int pos = 0u;
+	int result;
+
+	struct app_arg *opts =
+		(struct app_arg *)internal_struct_copy( TEST_ARGS,
+		sizeof( struct app_arg ),
+		sizeof( TEST_ARGS ) / sizeof( struct app_arg ) );
+	argv = internal_args_allocate( args, argc );
+
+	result = app_arg_parse( opts, argc, argv, &pos );
+	assert_int_equal( result, EXIT_FAILURE );
+
+	internal_struct_free( opts );
+	internal_args_free( argv, argc );
+}
+
+
+/* app_arg_usage */
+static void test_app_arg_usage_null_obj( void **state )
+{
+	app_arg_usage( NULL, 10u, "app name", "app description",
+		"pos_id", "positional argument description" );
+	/* above function shouldn't crash */
+	assert_true( 1 );
+}
+
+static void test_app_arg_usage_null_app_description( void **state )
+{
+	app_arg_usage( TEST_ARGS, 10u, "app name", NULL,
+		"pos_id", "positional argument description" );
+	/* above function shouldn't crash */
+	assert_true( 1 );
+}
+
+static void test_app_arg_usage_null_app_name( void **state )
+{
+	app_arg_usage( TEST_ARGS, 10u, NULL, "app description",
+		"pos_id", "positional argument description" );
+	/* above function shouldn't crash */
+	assert_true( 1 );
+}
+
+static void test_app_arg_usage_null_positional_name( void **state )
+{
+	app_arg_usage( TEST_ARGS, 10u, "app name", "app description",
+		NULL, "positional argument description" );
+	/* above function shouldn't crash */
+	assert_true( 1 );
+}
+
+static void test_app_arg_usage_null_positional_description( void **state )
+{
+	app_arg_usage( TEST_ARGS, 20u, "path/to/app", "app description",
+		"pos_id", NULL );
+	/* above function shouldn't crash */
+	assert_true( 1 );
+}
+
+static void test_app_arg_usage_positional_name_multi( void **state )
+{
+	app_arg_usage( TEST_ARGS, 20u, "path/to/app", "app description",
+		"pos_id+", "positional argument description" );
+	/* above function shouldn't crash */
+	assert_true( 1 );
+}
+
+static void test_app_arg_usage_positional_name_multi_optional( void **state )
+{
+	app_arg_usage( TEST_ARGS, 20u, "path/to/app", "app description",
+		"[pos_id]+", "positional argument description" );
+	/* above function shouldn't crash */
+	assert_true( 1 );
+}
+
+static void test_app_arg_usage_positional_name_optional( void **state )
+{
+	app_arg_usage( TEST_ARGS, 20u, "path/to/app", "app description",
+		"[pos_id]", "positional argument description" );
+	/* above function shouldn't crash */
+	assert_true( 1 );
+}
+
+static void test_app_arg_usage_positional_name_single( void **state )
+{
+	app_arg_usage( TEST_ARGS, 200u, "path/to/app", "app description",
+		"pos_id", "positional argument description" );
+	/* above function shouldn't crash */
+	assert_true( 1 );
+}
+
+
+/* main */
+int main( int argc, char* argv[] )
+{
+	int result;
+	const struct CMUnitTest tests[] =
+	{
+		cmocka_unit_test( test_app_arg_count_found_by_id ),
+		cmocka_unit_test( test_app_arg_count_found_by_name ),
+		cmocka_unit_test( test_app_arg_count_no_id_or_name ),
+		cmocka_unit_test( test_app_arg_count_null_obj ),
+		cmocka_unit_test( test_app_arg_parse_argument_expected_value ),
+		cmocka_unit_test( test_app_arg_parse_argument_no_value ),
+		cmocka_unit_test( test_app_arg_parse_argument_optional_not_specified ),
+		cmocka_unit_test( test_app_arg_parse_argument_optional_specified ),
+		cmocka_unit_test( test_app_arg_parse_argument_optional_specified_twice ),
+		cmocka_unit_test( test_app_arg_parse_argument_required ),
+		cmocka_unit_test( test_app_arg_parse_argument_unexpected_value ),
+		cmocka_unit_test( test_app_arg_parse_argument_unknown_dash ),
+		cmocka_unit_test( test_app_arg_parse_argument_unknown_no_dash ),
+		cmocka_unit_test( test_app_arg_parse_missing_required ),
+		cmocka_unit_test( test_app_arg_parse_mulitple ),
+		cmocka_unit_test( test_app_arg_parse_null_obj ),
+		cmocka_unit_test( test_app_arg_parse_no_args ),
+		cmocka_unit_test( test_app_arg_usage_null_obj ),
+		cmocka_unit_test( test_app_arg_usage_null_app_name ),
+		cmocka_unit_test( test_app_arg_usage_null_app_description ),
+		cmocka_unit_test( test_app_arg_usage_null_app_name ),
+		cmocka_unit_test( test_app_arg_usage_null_positional_name ),
+		cmocka_unit_test( test_app_arg_usage_null_positional_description ),
+		cmocka_unit_test( test_app_arg_usage_positional_name_multi ),
+		cmocka_unit_test( test_app_arg_usage_positional_name_optional ),
+		cmocka_unit_test( test_app_arg_usage_positional_name_optional ),
+		cmocka_unit_test( test_app_arg_usage_positional_name_single ),
+	};
+	MOCK_SYSTEM_ENABLED = 1;
+	result = cmocka_run_group_tests( tests, NULL, NULL );
+	MOCK_SYSTEM_ENABLED = 0;
+	return result;
+}
+
